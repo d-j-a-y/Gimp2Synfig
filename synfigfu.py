660cac30df0888d59b60911c63fed41a9fb715a2
[d.j.a.y] Jerome Blanchi <d.j.a.y@free.fr>
2016-02-22 19:25:38 +0100

    synfigfu usable as a script
    * $ python synfigfu.py
    * do nothing API

--- synfigfu.py
+++ synfigfu.py
@@ -32,167 +32,173 @@ SYNFIG_BLEND_ALPHA_OVER=19
 
 class SynfigObject:
 	def __init__(self):
-
-############################################################
-# Synfig Studio layer xml templates
-############################################################
-		self.document_begin = """\
+	############################################################
+	# Synfig Studio layer xml templates
+	############################################################
+		self.document_begin = """
 <canvas version="0.3" width="%(width)i" height="%(height)i" xres="%(xres)f" yres="%(yres)f" view-box="-%(x)f %(y)f %(x)f -%(y)f" >
-  <name>%(name)s</name>
-"""
+        <name>%(name)s</name>
+    """
 		self.document_end = """\
 </canvas>
 """
 		self.layer_inline_begin = """\
   <layer type="PasteCanvas" active="%(active)s" version="0.1" desc="%(name)s">
-    <param name="z_depth">
-      <real value="0.0000000000"/>
-    </param>
-    <param name="amount">
-      <real value="%(amount)f"/>
-    </param>
-    <param name="blend_method">
-      <integer value="%(blend_method)i"/>
-    </param>
-    <param name="origin">
-      <vector>
-        <x>%(x)f</x>
-        <y>%(y)f</y>
-      </vector>
-    </param>
-    <param name="canvas">
-      <canvas xres="10.000000" yres="10.000000">
+	<param name="z_depth">
+	  <real value="0.0000000000"/>
+	</param>
+	<param name="amount">
+	  <real value="%(amount)f"/>
+	</param>
+	<param name="blend_method">
+	  <integer value="%(blend_method)i"/>
+	</param>
+	<param name="origin">
+	  <vector>
+		<x>%(x)f</x>
+		<y>%(y)f</y>
+	  </vector>
+	</param>
+	<param name="canvas">
+	  <canvas xres="10.000000" yres="10.000000">
 """
 		self.layer_inline_end = """\
-      </canvas>
-    </param>
+	  </canvas>
+	</param>
   </layer>
 """
 		self.layer_image = """\
-        <layer type="import" active="true" version="0.1" desc="%(name)s">
-          <param name="z_depth">
-            <real value="0.0000000000"/>
-          </param>
-          <param name="amount">
-            <real value="%(amount)f"/>
-          </param>
-          <param name="blend_method">
-            <integer value="%(blend_method)i"/>
-          </param>
-          <param name="tl">
-            <vector>
-              <x>-%(x)f</x>
-              <y>%(y)f</y>
-            </vector>
-          </param>
-          <param name="br">
-            <vector>
-              <x>%(x)f</x>
-              <y>-%(y)f</y>
-            </vector>
-          </param>
-          <param name="filename">
-            <string>%(file)s</string>
-          </param>
-        </layer>
+		<layer type="import" active="true" version="0.1" desc="%(name)s">
+		  <param name="z_depth">
+			<real value="0.0000000000"/>
+		  </param>
+		  <param name="amount">
+			<real value="%(amount)f"/>
+		  </param>
+		  <param name="blend_method">
+			<integer value="%(blend_method)i"/>
+		  </param>
+		  <param name="tl">
+			<vector>
+			  <x>-%(x)f</x>
+			  <y>%(y)f</y>
+			</vector>
+		  </param>
+		  <param name="br">
+			<vector>
+			  <x>%(x)f</x>
+			  <y>-%(y)f</y>
+			</vector>
+		  </param>
+		  <param name="filename">
+			<string>%(file)s</string>
+		  </param>
+		</layer>
 """
 		self.layer_zoom = """
-        <layer type="zoom" active="true" version="0.1" desc="%(name)s">
-          <param name="amount">
-            <real value="0.0000000000"/>
-          </param>
-          <param name="center">
-            <vector>
-              <x>0.0000000000</x>
-              <y>0.0000000000</y>
-            </vector>
-          </param>
-        </layer>
+		<layer type="zoom" active="true" version="0.1" desc="%(name)s">
+		  <param name="amount">
+			<real value="0.0000000000"/>
+		  </param>
+		  <param name="center">
+			<vector>
+			  <x>0.0000000000</x>
+			  <y>0.0000000000</y>
+			</vector>
+		  </param>
+		</layer>
 """
 		self.layer_rotate = """
-        <layer type="rotate" active="true" version="0.1" desc="%(name)s">
-          <param name="origin">
-            <vector>
-              <x>0.0000000000</x>
-              <y>0.0000000000</y>
-            </vector>
-          </param>
-          <param name="amount">
-            <angle value="0.000000"/>
-          </param>
-        </layer>
+		<layer type="rotate" active="true" version="0.1" desc="%(name)s">
+		  <param name="origin">
+			<vector>
+			  <x>0.0000000000</x>
+			  <y>0.0000000000</y>
+			</vector>
+		  </param>
+		  <param name="amount">
+			<angle value="0.000000"/>
+		  </param>
+		</layer>
 """
 		self.layer_translate = """
-        <layer type="translate" active="true" version="0.1" desc="%(name)s">
-          <param name="origin">
-            <vector>
-              <x>0.0000000000</x>
-              <y>0.0000000000</y>
-            </vector>
-          </param>
-        </layer>
+		<layer type="translate" active="true" version="0.1" desc="%(name)s">
+		  <param name="origin">
+			<vector>
+			  <x>0.0000000000</x>
+			  <y>0.0000000000</y>
+			</vector>
+		  </param>
+		</layer>
 """
 		self.layer_switch_begin = """
-        <layer type="switch" active="%(active)s" exclude_from_rendering="false" version="0.0" desc="%(name)s">
-         <param name="z_depth">
-           <real value="0.0000000000"/>
-         </param>
-         <param name="amount">
-          <real value="%(amount)f"/>
-         </param>
-         <param name="blend_method">
-          <integer value="0" static="true"/>
-         </param>
-         <param name="origin">
-          <vector>
-            <x>0.0000000000</x>
-            <y>0.0000000000</y>
-          </vector>
-         </param>
-         <param name="transformation">
-          <composite type="transformation">
-           <offset>
-            <vector>
-              <x>0.0000000000</x>
-              <y>0.0000000000</y>
-            </vector>
-           </offset>
-           <angle>
-            <angle value="0.000000"/>
-           </angle>
-        <skew_angle>
-          <angle value="0.000000"/>
-        </skew_angle>
-        <scale>
-          <vector>
-            <x>1.0000000000</x>
-            <y>1.0000000000</y>
-          </vector>
-        </scale>
-        </composite>
-       </param>
-       <param name="canvas">
-        <canvas>
+		<layer type="switch" active="%(active)s" exclude_from_rendering="false" version="0.0" desc="%(name)s">
+		 <param name="z_depth">
+		   <real value="0.0000000000"/>
+		 </param>
+		 <param name="amount">
+		  <real value="%(amount)f"/>
+		 </param>
+		 <param name="blend_method">
+		  <integer value="0" static="true"/>
+		 </param>
+		 <param name="origin">
+		  <vector>
+			<x>0.0000000000</x>
+			<y>0.0000000000</y>
+		  </vector>
+		 </param>
+		 <param name="transformation">
+		  <composite type="transformation">
+		   <offset>
+			<vector>
+			  <x>0.0000000000</x>
+			  <y>0.0000000000</y>
+			</vector>
+		   </offset>
+		   <angle>
+			<angle value="0.000000"/>
+		   </angle>
+		<skew_angle>
+		  <angle value="0.000000"/>
+		</skew_angle>
+		<scale>
+		  <vector>
+			<x>1.0000000000</x>
+			<y>1.0000000000</y>
+		  </vector>
+		</scale>
+		</composite>
+	   </param>
+	   <param name="canvas">
+		<canvas>
 """
 		self.layer_switch_end = """
-      </canvas>
-    </param>
-    <param name="time_dilation">
-      <real value="1.0000000000"/>
-    </param>
-    <param name="time_offset">
-      <time value="0s"/>
-    </param>
-    <param name="children_lock">
-      <bool value="true" static="true"/>
-    </param>
-    <param name="outline_grow">
-      <real value="0.0000000000"/>
-    </param>
-    <param name="layer_name">
-      <string>%(name)s</string>
-    </param>
+	  </canvas>
+	</param>
+	<param name="time_dilation">
+	  <real value="1.0000000000"/>
+	</param>
+	<param name="time_offset">
+	  <time value="0s"/>
+	</param>
+	<param name="children_lock">
+	  <bool value="true" static="true"/>
+	</param>
+	<param name="outline_grow">
+	  <real value="0.0000000000"/>
+	</param>
+	<param name="layer_name">
+	  <string>%(name)s</string>
+	</param>
   </layer>
 """
 		self.name = 'synfig object'
 		self.version = '0.0 - draft'
+
+# Make usable as a script as well as an importable module
+if __name__ == "__main__":
+	import sys
+	synfigfumainscript = SynfigObject()
+	print ('synfigfu !\n*do nothing API*\n(draft proof of concept mode used by synfigexport.py Gimp to Synfig plugins)\n\n' + synfigfumainscript.name + ' (version : ' + synfigfumainscript.version + ')')
+#	synfigfu(int(sys.argv[1]))
